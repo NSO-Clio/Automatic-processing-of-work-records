35238cb0723890bde7c9fc7be2c8f4a26c0305fe
Egor Andreasyan <124351915+EgorAndrik@users.noreply.github.com>
2024-05-17 21:27:51 +0700

    Create get_boxes.py

---
+++ get_boxes.py
@@ -0,0 +1,11 @@
+import keras_ocr
+
+
+class GetTextBoxes:
+    def __init__(self) -> None:
+        self.pipeline = keras_ocr.pipeline.Pipeline()
+    
+    def get_box(self, img_path: str) -> list:
+        image = keras_ocr.tools.read(img_path)
+        predictions = pipeline.recognize([image])[0]
+        return [box for _, box in predictions]
