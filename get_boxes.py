b8b58312f1a49aa577646db72856400fa9bdcff8
Egor Andreasyan <124351915+EgorAndrik@users.noreply.github.com>
2024-05-17 22:02:12 +0700

    Update get_boxes.py

--- get_boxes.py
+++ get_boxes.py
@@ -21,3 +21,29 @@ class GetTextBoxes:
         image = keras_ocr.tools.read(img_path)  # Чтение изображения с указанного пути
         predictions = self.pipeline.recognize([image])[0]  # Распознавание текста на изображении
         return [box for _, box in predictions]  # Возврат списка координат текстовых областей
+
+    def crop_rectangle(self, image: np.ndarray, box: np.ndarray) -> np.ndarray:
+        """
+        Вырезает прямоугольник из изображения по координатам четырех углов.
+
+        Параметры:
+        image (np.ndarray): Изображение, из которого нужно вырезать прямоугольник.
+        box (np.ndarray): Массив координат четырех углов прямоугольника.
+
+        Возвращает:
+        np.ndarray: Вырезанный прямоугольник.
+        """
+        # Находим размер вырезаемого прямоугольника
+        width = int(np.linalg.norm(box[0] - box[1]))
+        height = int(np.linalg.norm(box[1] - box[2]))
+
+        # Определяем целевые точки (прямоугольник с найденными размерами)
+        dst_pts = np.array([[0, 0], [width, 0], [width, height], [0, height]], dtype="float32")
+
+        # Вычисляем матрицу перспективного преобразования
+        M = cv2.getPerspectiveTransform(box.astype("float32"), dst_pts)
+
+        # Применяем преобразование и вырезаем прямоугольник
+        cropped_image = cv2.warpPerspective(image, M, (width, height))
+
+        return cropped_image
