2559e7f18ae0693ca192ac7b213c0763960f826f
Egor Andreasyan <124351915+EgorAndrik@users.noreply.github.com>
2024-05-17 21:34:42 +0700

    Update get_boxes.py add coments

--- get_boxes.py
+++ get_boxes.py
@@ -3,9 +3,21 @@ import keras_ocr
 
 class GetTextBoxes:
     def __init__(self) -> None:
+        """
+        Инициализирует объект GetTextBoxes, создавая экземпляр конвейера для распознавания текста.
+        """
         self.pipeline = keras_ocr.pipeline.Pipeline()
     
     def get_box(self, img_path: str) -> list:
-        image = keras_ocr.tools.read(img_path)
-        predictions = self.pipeline.recognize([image])[0]
-        return [box for _, box in predictions]
+        """
+        Возвращает список текстовых областей (bounding boxes) для текста, найденного на изображении.
+
+        Параметры:
+        img_path (str): Путь к изображению, на котором нужно распознать текст.
+
+        Возвращает:
+        list: Список координат областей, содержащих текст.
+        """
+        image = keras_ocr.tools.read(img_path)  # Чтение изображения с указанного пути
+        predictions = self.pipeline.recognize([image])[0]  # Распознавание текста на изображении
+        return [box for _, box in predictions]  # Возврат списка координат текстовых областей
